05d583f46aff9d9dbecac3ac05aaa40b7047aaa3
Bharathi Sridhar <bharathisridhar04@gmail.com>
2023-01-21 18:06:45 -0500

    sppech to text

---
+++ speechtotext.py
@@ -0,0 +1,11 @@
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    # read the audio data from the default microphone
+    audio_data = r.record(source, duration=10)
+    print("Recognizing...")
+    # convert speech to text
+    text = r.recognize_google(audio_data)
+    print(text)
